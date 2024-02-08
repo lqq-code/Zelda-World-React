08ada02ee2311735446ec183ed1507315e849977
椰卤工程师 <61107184+lqq-code@users.noreply.github.com>
2024-02-08 16:02:43 +1100

    feat: add map 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Routes from './Routes'; 
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
     <Routes />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
